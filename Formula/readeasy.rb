e642482314d5ae606d797a8640cacbc4e45cefd2
Jason Pierrot <58733091+ItsJasonPierrot@users.noreply.github.com>
2026-09-22 21:26:36 +0200

    readeasy 1.8.0

--- Formula/readeasy.rb
+++ Formula/readeasy.rb
@@ -1,8 +1,8 @@
 class Readeasy < Formula
   desc "Terminal text-to-speech reader that makes reading easier"
   homepage "https://github.com/ItsJasonPierrot/readeasy"
-  url "https://github.com/ItsJasonPierrot/readeasy/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "4ba7644fef4e45e606c0e3a5c23478a677ffb81bf796081a2deb40ab2fc0b36e"
+  url "https://github.com/ItsJasonPierrot/readeasy/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "2d3368384c82180e111e3317f84f981aa4558e0d57d265b353052999e97ea6a4"
   license "MIT"
   head "https://github.com/ItsJasonPierrot/readeasy.git", branch: "master"
 
